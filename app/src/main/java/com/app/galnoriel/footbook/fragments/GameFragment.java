a0862a39e75cb62629814fc1d13b23e436c5096d
Ouriel91 <orieloh91@gmail.com>
2019-07-05 19:06:01 +0300

    Timer - done, need to take care in reset button service and notfication when finished timer will be added

--- app/src/main/java/com/app/galnoriel/footbook/fragments/GameFragment.java
+++ app/src/main/java/com/app/galnoriel/footbook/fragments/GameFragment.java
@@ -1,6 +1,8 @@
 package com.app.galnoriel.footbook.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
@@ -9,10 +11,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.ToggleButton;
 
 import com.app.galnoriel.footbook.MainActivity;
@@ -22,13 +26,18 @@ import java.util.Locale;
 
 public class GameFragment extends Fragment implements View.OnClickListener, View.OnLongClickListener {
 
-    View view;
+    private View view;
+
+    private ToggleButton start_timer;
+    private CountDownTimer mCountDownTimer;
 
     private long startTimeInMillis;
-    private long leftTimeInMillis;
+    private long leftTimeInMillis = 0;
     private long endTime;
+    private boolean isTimerRunning;
 
-    private EditText timer_view;
+    private EditText timer_et;
+    private TextView timer_tv;
 
     @Nullable
     @Override
@@ -46,73 +55,136 @@ public class GameFragment extends Fragment implements View.OnClickListener, View
 //endregion
 
 //region timer layout and listeners
-        final ToggleButton start_timer = view.findViewById(R.id.start_timer_gamf);
+        start_timer = view.findViewById(R.id.start_timer_gamf);
         ImageButton reset_timer = view.findViewById(R.id.reset_timer_gamf);
-        timer_view = view.findViewById(R.id.timer_view);
+        timer_et = view.findViewById(R.id.timer_et);
+        timer_tv = view.findViewById(R.id.timer_tv);
 //endregion
 
         start_timer.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
-                String input = timer_view.getText().toString();
-                //validation region
-                if (input.length() == 0){
-                    Snackbar.make(MainActivity.coordinatorLayout,"Enter valid time",Snackbar.LENGTH_SHORT).show();
-                    start_timer.setChecked(false);
-                    return;
+                if (leftTimeInMillis == 0){
+                    String input = timer_et.getText().toString();
+                    Long millisInput = 0L;
+
+                    if (input.length() == 0){
+                        Snackbar.make(MainActivity.coordinatorLayout,"Enter valid time",Snackbar.LENGTH_SHORT).show();
+
+                        return;
+                    }
+
+                    try {
+                        millisInput = Long.parseLong(input) *60000;
+                    }catch (NumberFormatException e){
+
+                    }
+
+                    if (millisInput == 0){
+                        Snackbar.make(MainActivity.coordinatorLayout,"Enter positive time",Snackbar.LENGTH_SHORT).show();
+
+                        return;
+                    }
+                    setTime(millisInput);
+                    timer_et.setText("");
+                    timer_et.setVisibility(View.GONE);
                 }
 
-                Long millisInput = 0L;
-
-                try {
-                    millisInput = Long.parseLong(input) *6000;
-                }catch (NumberFormatException e){
-
-                }
-
-                if (millisInput == 0){
-                    Snackbar.make(MainActivity.coordinatorLayout,"Enter positive time",Snackbar.LENGTH_SHORT).show();
-                    start_timer.setChecked(false);
-                    return;
-                }
-                //end region
-
-
-                setTime(millisInput);
                 if (isChecked){ //play
 
-
-                    timer_view.setEnabled(false);
-                }
-                else {
+                    //timer_et.setVisibility(View.GONE);
+                    startTimer();
 
                 }
+                else { //pause
+                    pauseTimer();
+                }
+            }
+        });
+
+        reset_timer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timer_et.setVisibility(View.VISIBLE);
+                resetTimer();
             }
         });
 
         return view;
     }
 
+    private void startTimer() {
+
+        endTime = System.currentTimeMillis() + leftTimeInMillis;
+
+        mCountDownTimer = new CountDownTimer(leftTimeInMillis,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+                leftTimeInMillis = millisUntilFinished;
+                updateCountdown();
+            }
+
+            @Override
+            public void onFinish() {
+
+                //isTimerRunning = false;
+                start_timer.setChecked(false);
+                updateWatchInterface();
+                //need to add notification
+
+            }
+        }.start();
+
+        start_timer.setChecked(true);
+        updateWatchInterface();
+    }
+
+    private void pauseTimer() {
+
+        mCountDownTimer.cancel();
+        start_timer.setChecked(false);
+        updateWatchInterface();
+    }
+
     private void setTime(Long milliSeconds) {
 
         startTimeInMillis = milliSeconds;
         resetTimer();
-        //closeKeyboard();
+        closeKeyboard();
     }
 
     private void resetTimer() {
         leftTimeInMillis = startTimeInMillis;
         updateCountdown();
-        //updateWatchInterface();
+        updateWatchInterface();
+    }
+
+    private void closeKeyboard() {
+
+        //closing keyboard after enter time
+
+        View view = getActivity().getCurrentFocus();
+        if (view != null){
+
+            InputMethodManager methodManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            methodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    private void updateWatchInterface() {
+
     }
 
     private void updateCountdown() {
+
         int hours = (int) (leftTimeInMillis / 1000) / 3600;
         int minutes = (int) ((leftTimeInMillis / 1000) % 3600) / 60;
         int seconds = (int) (leftTimeInMillis / 1000) % 60;
 
         String timeLeftFormatted;
+
         if (hours > 0) {
             timeLeftFormatted = String.format(Locale.getDefault(),
                     "%d:%02d:%02d", hours, minutes, seconds);
@@ -121,7 +193,7 @@ public class GameFragment extends Fragment implements View.OnClickListener, View
                     "%02d:%02d", minutes, seconds);
         }
 
-        timer_view.setText(timeLeftFormatted);
+        timer_tv.setText(timeLeftFormatted);
     }
 
     @Override
