311a40f229e84e0b4927371669c2ccd88178362f
Gal <galk88@gmail.com>
2019-07-06 21:05:50 +0300

    change layouts

--- app/src/main/java/com/app/galnoriel/footbook/fragments/ProfileFragment.java
+++ app/src/main/java/com/app/galnoriel/footbook/fragments/ProfileFragment.java
@@ -1,6 +1,8 @@
 package com.app.galnoriel.footbook.fragments;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -12,6 +14,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -31,13 +34,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class ProfileFragment extends Fragment implements MainToFrag {
+public class ProfileFragment extends Fragment implements MainToFrag, View.OnClickListener {
 //region declarations
     private RecyclerView profileRV;
     private CustomSharedPrefAdapter sPref;
-    TextView nameTV,whereFromTV,positionTV;
-    Spinner pitchSP,regionSP;
-    ImageView pitchIV,chatIV;
+    TextView nameTV,whereFromTV,positionTV, pitchTV, wherePlayTV;
+    ImageView pitchIV,chatIV,positionIV;
     List<GroupPlay> groupPlayList;
     public MoveToTab showTab;
     public UpdateGroupDB updateGroupDB;
@@ -68,15 +70,23 @@ public class ProfileFragment extends Fragment implements MainToFrag {
         // region var assignments
         profileRV = view.findViewById(R.id.profile_rv);
         nameTV = view.findViewById(R.id.name_tv_prf);
-        whereFromTV = view.findViewById(R.id.region_tv_prf);
+        whereFromTV = view.findViewById(R.id.where_from_tv_prf);
         positionTV = view.findViewById(R.id.position_tv_prf);
-        pitchSP = view.findViewById(R.id.pitch_tv_prf);
-        regionSP = view.findViewById(R.id.where_play_tv_prf);
+        pitchTV = view.findViewById(R.id.pitch_tv_prf);
+        wherePlayTV = view.findViewById(R.id.where_play_tv_prf);
         chatIV = view.findViewById(R.id.msg_btn_prf);
         pitchIV = view.findViewById(R.id.pitch_ic_prf);
+        positionIV = view.findViewById(R.id.position_ic_prf);
         //endregion
 
         //region assign views click listeners
+        nameTV.setOnClickListener(this);
+        whereFromTV.setOnClickListener(this);
+        wherePlayTV.setOnClickListener(this);
+        positionTV.setOnClickListener(this);
+        pitchTV.setOnClickListener(this);
+
+
             //add groups:
         view.findViewById(R.id.groups_title_lay_prf).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -173,4 +183,50 @@ public class ProfileFragment extends Fragment implements MainToFrag {
     }
 
 
+    @Override
+    public void onClick(View v) { //clicked on text view to edit it
+        String hint ="";
+        Resources r = getResources();
+        Drawable img = r.getDrawable(R.drawable.profile_ic_tab);
+        switch (v.getId()){
+            case R.id.name_tv_prf: hint = r.getString(R.string.full_name); break;
+            case R.id.pitch_tv_prf: img = r.getDrawable(R.drawable.football_field_ic);  break;
+            case R.id.position_tv_prf:img = r.getDrawable(R.drawable.striker); break;
+            case R.id.where_from_tv_prf:
+                hint = r.getString(R.string.address);
+                img = r.getDrawable(R.drawable.location);
+                break;
+            case R.id.where_play_tv_prf: img = r.getDrawable(R.drawable.where); break;
+        }
+
+        //make alert dialog pop dynamically
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        final View dialogSignView = getLayoutInflater().inflate(R.layout.dialog_create_group,null);
+        builder.setView(dialogSignView);
+        final AlertDialog dialog = builder.create();
+        final EditText editText = dialogSignView.findViewById(R.id.text_edit_dialog);
+        final Spinner spinner = dialogSignView.findViewById(R.id.spinner_edit_dialog);
+        final ImageView icon = dialogSignView.findViewById(R.id.image_edit_dialog);
+        if (!hint.isEmpty())editText.setHint(hint);
+        icon.setImageDrawable(img);
+
+
+        dialogSignView.findViewById(R.id.confirm_btn_edit_dialog).setOnClickListener(new View.OnClickListener() {
+            //create button will create group on server and move to group frag
+            @Override
+            public void onClick(View v) {
+                String newValue = editText.getText().toString();
+                ((TextView)v).setText(newValue);
+                dialog.dismiss();
+            }
+        });
+
+
+        dialog.show();
+
+    }
+
+    private void editProfileDisplay(int view_id,String value){
+//        getTargetFragment()
+    }
 }
